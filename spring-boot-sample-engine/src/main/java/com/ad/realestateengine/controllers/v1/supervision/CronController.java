c0f59b5b2435ffab90d98df777f17b51d134fddb
Adrien Durot <adriendurot3@hotmail.com>
2021-04-28 21:33:11 +0200

    Changed sonarExclusions

--- spring-boot-sample-engine/src/main/java/com/ad/realestateengine/controllers/v1/supervision/CronController.java
+++ spring-boot-sample-engine/src/main/java/com/ad/realestateengine/controllers/v1/supervision/CronController.java
@@ -1,7 +1,6 @@
 package com.ad.realestateengine.controllers.v1.supervision;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +14,6 @@ import io.swagger.annotations.ApiResponses;
 
 
 @RestController
-@CrossOrigin
 @Api(value = "Goup APIs for the Cron Jobs", tags = "Crons")
 public class CronController extends V1Controller{
 	
